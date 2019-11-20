d1bfbdca85f967e06cf072cc21d20c6738a32a9e
Stephen Welch <stephenwelchva@gmail.com>
2019-11-20 02:53:07 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/glad.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/code/learn-opengl/include/glad/src/glad.c" "C:/code/learn-opengl/cmake-build-debug/CMakeFiles/glad.dir/include/glad/src/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"C:/code/learn-opengl\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/glad/include"
+  "../include/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/code/learn-opengl/include/glfw/src/context.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/context.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/egl_context.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/egl_context.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/init.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/init.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/input.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/input.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/monitor.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/monitor.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/osmesa_context.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/vulkan.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/vulkan.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/wgl_context.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/wgl_context.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_init.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_init.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_joystick.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_joystick.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_monitor.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_monitor.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_thread.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_thread.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_time.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_time.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/win32_window.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/win32_window.c.obj"
+  "C:/code/learn-opengl/include/glfw/src/window.c" "C:/code/learn-opengl/cmake-build-debug/include/glfw/src/CMakeFiles/glfw.dir/window.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_CRT_SECURE_NO_WARNINGS"
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/glfw/include"
+  "../include/glfw/src"
+  "include/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
