d63d0f43725bec1976dd135a4e22b4ebb3d8f8f8
Stephen Welch <stephenwelchva@gmail.com>
2019-12-26 22:39:48 -0500

    Add #defines for logging methods

--- src/util/Log.h
+++ src/util/Log.h
@@ -9,3 +9,10 @@ public:
 private:
   static std::shared_ptr<spdlog::logger> logger;
 };
+
+#define LOG_TRACE(...)    Log::getLogger()->trace(__VA_ARGS__)
+#define LOG_DEBUG(...)    Log::getLogger()->debug(__VA_ARGS__)
+#define LOG_INFO(...)     Log::getLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)     Log::getLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)    Log::getLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...) Log::getLogger()->critical(__VA_ARGS__)
