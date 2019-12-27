cedbf4c357e0effc1b15396b65faca28033ff4a9
Stephen Welch <stephenwelchva@gmail.com>
2019-12-26 20:47:08 -0500

    Refactor debug output to use logger

--- src/graphics/Window.cpp
+++ src/graphics/Window.cpp
@@ -44,7 +44,6 @@ bool Window::init() {
   // Selects this window for any future OpenGL calls
   glfwMakeContextCurrent(window);
 
-
   // Initialize GLAD before calling OpenGL
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     Log::getLogger()->error("Failed to initialize GLAD");
@@ -206,76 +205,75 @@ void Window::glDebugOutput(unsigned int source, unsigned int type,
   // ignore non-significant error/warning codes
   if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
 
-  std::cout << "---------------" << std::endl;
-  std::cout << "Debug message (" << id << "): " << message << std::endl;
+  std::string logMessage, sourceMessage, typeMessage, severityMessage;
 
   switch (source) {
   case GL_DEBUG_SOURCE_API:
-    std::cout << "Source: API";
+    sourceMessage = "Source: API";
     break;
   case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-    std::cout << "Source: Window System";
+    sourceMessage = "Source: Window System";
     break;
   case GL_DEBUG_SOURCE_SHADER_COMPILER:
-    std::cout << "Source: Shader Compiler";
+    sourceMessage = "Source: Shader Compiler";
     break;
   case GL_DEBUG_SOURCE_THIRD_PARTY:
-    std::cout << "Source: Third Party";
+    sourceMessage = "Source: Third Party";
     break;
   case GL_DEBUG_SOURCE_APPLICATION:
-    std::cout << "Source: Application";
+    sourceMessage = "Source: Application";
     break;
   case GL_DEBUG_SOURCE_OTHER:
-    std::cout << "Source: Other";
+    sourceMessage = "Source: Other";
     break;
   }
-  std::cout << std::endl;
 
   switch (type) {
   case GL_DEBUG_TYPE_ERROR:
-    std::cout << "Type: Error";
+    typeMessage = "Type: Error";
     break;
   case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-    std::cout << "Type: Deprecated Behaviour";
+    typeMessage = "Type: Deprecated Behaviour";
     break;
   case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-    std::cout << "Type: Undefined Behaviour";
+    typeMessage = "Type: Undefined Behaviour";
     break;
   case GL_DEBUG_TYPE_PORTABILITY:
-    std::cout << "Type: Portability";
+    typeMessage = "Type: Portability";
     break;
   case GL_DEBUG_TYPE_PERFORMANCE:
-    std::cout << "Type: Performance";
+    typeMessage = "Type: Performance";
     break;
   case GL_DEBUG_TYPE_MARKER:
-    std::cout << "Type: Marker";
+    typeMessage = "Type: Marker";
     break;
   case GL_DEBUG_TYPE_PUSH_GROUP:
-    std::cout << "Type: Push Group";
+    typeMessage = "Type: Push Group";
     break;
   case GL_DEBUG_TYPE_POP_GROUP:
-    std::cout << "Type: Pop Group";
+    typeMessage = "Type: Pop Group";
     break;
   case GL_DEBUG_TYPE_OTHER:
-    std::cout << "Type: Other";
+    typeMessage = "Type: Other";
     break;
   }
-  std::cout << std::endl;
 
   switch (severity) {
   case GL_DEBUG_SEVERITY_HIGH:
-    std::cout << "Severity: high";
+    severityMessage = "Severity: high";
     break;
   case GL_DEBUG_SEVERITY_MEDIUM:
-    std::cout << "Severity: medium";
+    severityMessage = "Severity: medium";
     break;
   case GL_DEBUG_SEVERITY_LOW:
-    std::cout << "Severity: low";
+    severityMessage = "Severity: low";
     break;
   case GL_DEBUG_SEVERITY_NOTIFICATION:
-    std::cout << "Severity: notification";
+    severityMessage = "Severity: notification";
     break;
   }
-  std::cout << std::endl;
-  std::cout << std::endl;
+
+  Log::getLogger()->error("\nDebug message ({}): \n{}\n{}\n{}\n{}", id, message, sourceMessage, typeMessage, severityMessage);
+
+
 }
