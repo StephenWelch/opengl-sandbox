909eebd1a81bd9790a6c0c905a8a568c2419930c
Stephen Welch <stephenwelchva@gmail.com>
2020-08-20 03:16:32 -0400

    Add Layer classes from Hazel

---
+++ src/engine/layer/Layer.cpp
@@ -0,0 +1,11 @@
+
+#include "Layer.h"
+
+namespace Hazel {
+
+	Layer::Layer(const std::string& debugName)
+		: m_DebugName(debugName)
+	{
+	}
+	
+}
---
+++ src/engine/layer/Layer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <chrono>
+#include "engine/Core.h"
+#include "engine/event/Event.h"
+
+namespace Hazel {
+
+	class Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() = default;
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate(std::chrono::milliseconds ts) {}
+		virtual void OnImGuiRender() {}
+		virtual void OnEvent(Event& event) {}
+
+		const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+}
