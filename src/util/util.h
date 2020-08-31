acd0371ffb3dcd945e05730cc888ae8b04fb4392
Stephen Welch <stephenwelchva@gmail.com>
2020-08-31 00:34:58 -0400

    Add hash utilities and remove cube generator

--- src/util/util.h
+++ src/util/util.h
@@ -3,8 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <engine/Core.h>
 #include <glad/glad.h>
-#include <graphics/Mesh.h>
-#include <stdio.h> /* defines FILENAME_MAX */
+#include <cstdio> /* defines FILENAME_MAX */
 
 #include <fstream>
 #include <iostream>
@@ -26,11 +25,39 @@
 
 #define BIT_AT(x) (1 << x)
 
+#define MAKE_HASHABLE(type, ...) \
+    namespace std {\
+        template<> struct hash<type> {\
+            std::size_t operator()(const type &t) const {\
+                std::size_t ret = 0;\
+                util::hash_combine(ret, __VA_ARGS__);\
+                return ret;\
+            }\
+        };\
+    }
+
 namespace util {
 
-std::shared_ptr<Mesh> generateCube(float sideLength,
-																	 unsigned int usage,
-																	 const std::vector<std::shared_ptr<Texture>> &textures);
+// https://stackoverflow.com/questions/2590677/how-do-i-combine-hash-values-in-c0x
+inline void hash_combine(std::size_t& seed) { }
+
+template <typename T, typename... Rest>
+inline void hash_combine(std::size_t& seed, const T& v, Rest... rest) {
+	std::hash<T> hasher;
+	seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+	hash_combine(seed, rest...);
+}
+
+// https://stackoverflow.com/questions/61030383/how-to-convert-stdfilesystemfile-time-type-to-time-t
+template <typename TP>
+std::time_t to_time_t(TP tp)
+{
+	using namespace std::chrono;
+	auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+																													+ system_clock::now());
+	return system_clock::to_time_t(sctp);
+}
+
 std::string getWorkingDirectory();
 std::string getFileAsString(const std::string &path);
 GLenum glCheckError_(const char *file, int line);
